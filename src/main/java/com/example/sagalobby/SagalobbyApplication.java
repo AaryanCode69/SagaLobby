2bb0c8116a8ee384dff5552f12e2b7b7f344dab6
AaryanCode69 <aaryanupadhyay68@gmail.com>
2026-05-19 20:41:58 +0530

    core: setup

---
+++ src/main/java/com/example/sagalobby/SagalobbyApplication.java
@@ -0,0 +1,13 @@
+package com.example.sagalobby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagalobbyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagalobbyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sagalobby/SagalobbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sagalobby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagalobbyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
